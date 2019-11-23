e1d97d22d0cbea8bbf63f1bb74f4a4df8e64cb0b
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2019-11-22 17:54:27 -0800

    Added configuration files for move_base and offset src

--- src/move_turtlebot/src/set_turtlebot_goal.py
+++ src/move_turtlebot/src/set_turtlebot_goal.py
@@ -4,16 +4,17 @@ import rospy
 import numpy as np
 import sys
 from velocity_controller import VelocityController, vector
-from geometry_msgs.msg import Twist, PoseStamped, Pose
+from geometry_msgs.msg import Twist, PoseStamped, Pose, Point, Quaternion
 import tf.transformations as tft
 
 def controller(marker_num, self_num, master_num):
 	vel_control = VelocityController(marker_num, self_num, master_num)
-	pub_target = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
+	pub_target = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10, latch=True)
 	#pub_rel_vel = rospy.Publisher(vel_control.turtlebot+'/relative_velocity', Twist, queue_size=10)
-	rate = rospy.Rate(10) #subject to change
+	rate = rospy.Rate(1) #subject to change
 	pose_target = Pose()
-	pose_target.position.z = 1
+	pose_target.position.x = 0.55
+	pose_target.position.z = -0.35
 	# pose_target.position.y = 0
 	# post_target.position.z = 0
 	# post_target.orientation.x 
@@ -22,12 +23,12 @@ def controller(marker_num, self_num, master_num):
 	pose_target.orientation.w = 1/np.sqrt(2)
 
 	target_stamped = PoseStamped()
+	target_stamped.header.stamp = rospy.Time.now()
 	target_stamped.header.frame_id = "map"
 	target_stamped.pose = None
 
-        while not target_stamped.pose:
+	while not target_stamped.pose:
 		target_stamped.pose = vel_control.transform_pose(pose_target, "map", vel_control.marker_frame)
-		rate.sleep()
 
 	target_stamped.pose.position.z = 0
 	angles = tft.euler_from_quaternion(vector(target_stamped.pose.orientation))
@@ -37,9 +38,13 @@ def controller(marker_num, self_num, master_num):
 	target_stamped.pose.orientation.z = quatern[2]
 	target_stamped.pose.orientation.w = quatern[3]
 
+        #position = Point(1.266, 0.0, 0.0)
+        #orientation = Quaternion(0, 0, 0.127, 0.991)
+        #target_stamped.pose.position = position
+        #target_stamped.pose.orientation = orientation
+
 	print(target_stamped)
 	pub_target.publish(target_stamped)
-
 	rospy.spin()
 
 if __name__ == '__main__':
