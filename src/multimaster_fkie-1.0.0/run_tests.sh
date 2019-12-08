797171fa636462ec3189b142fb696bb42994f3f2
Class Account <gareyflee@gmail.com>
2019-12-07 18:50:18 -0800

    Deleted multimaster fkie because we are now using namespaces.

--- src/multimaster_fkie-1.0.0/run_tests.sh
+++
@@ -1,2 +0,0 @@
-nosetests -s -vv -w master_discovery_fkie/tests/ --with-coverage --cover-erase --nocapture --cover-package=master_discovery_fkie
-nosetests -s -vv -w node_manager_daemon_fkie/tests/ --with-coverage --cover-erase --nocapture --cover-package=node_manager_daemon_fkie
