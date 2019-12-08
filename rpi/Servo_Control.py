470bbd1cd7aac39dd403dec86e1e54b5a6fb661e
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2019-12-08 11:03:38 -0800

    Added comments to Servo_Control and tested it out

--- rpi/Servo_Control.py
+++ rpi/Servo_Control.py
@@ -1,27 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
-
-
-
 def Setup(*pins):
     servos = []
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     # use GPIO.BCM for SOC channel numbers
     # use GPIO.BOARD for printed board numbers
     for pin in pins:
         servo = Servo(pin)
         servos.append(servo)
     return servos
+
 class Servo:
+
     def __init__(self, pinNum, startAngle=90, frequency=50):
         GPIO.setup(pinNum, GPIO.OUT)
         self.p = GPIO.PWM(pinNum, 50)
-        self.p.start(startAngle*10/18)
+        #self.p.start(startAngle*10/18)
+
     def update_angle(self, angle):
+        # Deprecated - do not use
         try:
-            # self.p.ChangeDutyCycle(angle*10/18)
-            self.p.ChangeDutyCycle(10)
+            self.p.ChangeDutyCycle(angle*10/18)
+            #self.p.ChangeDutyCycle(10)
             time.sleep(angle*0.2/60*1.1) # Turning speed from data sheet 
         except:
             print("Exception...")
@@ -29,12 +30,14 @@ class Servo:
             GPIO.cleanup()
 
 def main():
-    servos = Setup(17)
+    #TODO Control servo between duty cycle 6 (delivery) and 10 (hold package). 7.5 is neutral level
+    # Not using angle conversion
+    servos = Setup(7)
     print("setup ", len(servos), " servo motors...")
-    servos[0].update_angle(5)
-    servos[0].update_angle(9)
-
-
+    servos[0].p.start(0)
+    while True:
+	val = float(input("Val: "))
+        servos[0].p.ChangeDutyCycle(val)
 
 if __name__ == "__main__":
     main()
