8859bfabacc67058be190b1b689ea97dff8d7297
Toby Baker <toby_baker@berkeley.edu>
2019-12-09 15:47:44 -0800

    modified path planner to be an object and publish using new message types

--- src/path_planner/src/path_planner.py
+++ src/path_planner/src/path_planner.py
@@ -6,17 +6,17 @@ from scipy.optimize import NonlinearConstraint
 import json
 import rospy
 from std_msgs.msg import String
+from path_planner.msg import NavigationTargets
 
-Node = "path_planner"
-Path_Plan_Topic = 'path_plan'
-TB_Seperation_Dist = 0.75
-
-master_start = (0, -1.3, 0)
-master_goal = (0, 0.9, 0)
-slave_start = (1, -1.25, 0)
-slave_goal = (1, 1, 0)
-
-min_drop_dist = 1.0
+# Path_Plan_Topic = 'path_plan'
+# TB_Seperation_Dist = 0.75
+#
+# master_start = (0, -1.3, 0)
+# master_goal = (0, 0.9, 0)
+# slave_start = (1, -1.25, 0)
+# slave_goal = (1, 1, 0)
+#
+# min_drop_dist = 1.0
 
 
 
@@ -24,7 +24,7 @@ def total_dist(x, p):
 	# All points are of form (x,y)
 	# x is a flattened list of:
 	# 		[drop start, drop end]
-	# p is a flattened list of: 
+	# p is a flattened list of:
 	#		[master start, master end, slave start, slave end]
 	x1 = np.array(x[0:2])
 	x2 = np.array(x[2:4])
@@ -70,21 +70,21 @@ def get_path_angle(start, end):
 	path_vector = np.subtract(end, start)
 	return np.round(np.arctan2(path_vector[1], path_vector[0])*180.0/np.pi, 1)
 
-def path_planner(master_start, master_goal, slave_start, slave_goal, drop_distance):
-	# All locations are of the form (x,y,theta) or (x,y)
-	# Drop distance is in meters
-	drop_start = [(master_start[0] - slave_start[0])/2, (master_start[1] - slave_start[1])/2]
-	drop_end = [drop_start[0] + drop_distance, drop_start[1]]
-	X = points_to_list(drop_start, drop_end)
-	P = points_to_list(master_start[0:2], master_goal[0:2], slave_start[0:2], slave_goal[0:2])
-
-	nl_cons = NonlinearConstraint(non_linear_dist_constraint, drop_distance, 100)
-	options = {'disp':False}
-	ans = minimize(fun=total_dist, args=P, x0=X, method='trust-constr', constraints=nl_cons, options=options)
-	# print(total_dist(ans.x,P))
-	angle = get_path_angle(ans.x[0:2], ans.x[2:4])
-
-	return (ans.x[0], ans.x[1], angle), (ans.x[2], ans.x[3], angle)
+# def path_planner(master_start, master_goal, slave_start, slave_goal, drop_distance):
+# 	# All locations are of the form (x,y,theta) or (x,y)
+# 	# Drop distance is in meters
+# 	drop_start = [(master_start[0] - slave_start[0])/2, (master_start[1] - slave_start[1])/2]
+# 	drop_end = [drop_start[0] + drop_distance, drop_start[1]]
+# 	X = points_to_list(drop_start, drop_end)
+# 	P = points_to_list(master_start[0:2], master_goal[0:2], slave_start[0:2], slave_goal[0:2])
+#
+# 	nl_cons = NonlinearConstraint(non_linear_dist_constraint, drop_distance, 100)
+# 	options = {'disp':False}
+# 	ans = minimize(fun=total_dist, args=P, x0=X, method='trust-constr', constraints=nl_cons, options=options)
+# 	# print(total_dist(ans.x,P))
+# 	angle = get_path_angle(ans.x[0:2], ans.x[2:4])
+#
+# 	return (ans.x[0], ans.x[1], angle), (ans.x[2], ans.x[3], angle)
 
 def plot_path(master_start, master_goal, slave_start, slave_goal, drop_start, drop_end):
 	# Plotting utility to visualize the 2d path calulated
@@ -115,54 +115,102 @@ def offset_coords(point, angle):
 	# Point is a coordinate in the form (x, y, theta)
 	delta = (TB_Seperation_Dist * np.cos(angle * np.pi/180), TB_Seperation_Dist * np.sin(angle * np.pi/180), 0)
 	return np.subtract(point, delta).tolist()
-	
+
 
 def points_to_list(*pts):
 	# Flattens an arbitrary number of points (x,y) into a list
 	return [i for sub in pts for i in sub]
 
-def publish_path_plan(path_start, path_end, master_start_pos, master_goal_pos, follower_start_pos, follower_end_pos):
-	# rospy.init_node(Node, anonymous=True)
-	angle = get_path_angle(path_start, path_end)
-	leader_start = path_start
-	leader_end = path_end
-	follower_start = offset_coords(path_start, angle)
-	follower_end = offset_coords(path_end, angle)
-	
-	msg_json_str = json.dumps({"Leader":{'Transfer_Start':leader_start, 'Transfer_End':leader_end, 
-								'Start':master_start_pos, 'End':master_goal_pos}, 
-								"Follower":{'Transfer_Start': follower_start, 'Transfer_End': follower_end, 
-								'Start': follower_start_pos, 'End': follower_end_pos}
-	})
-	pub = rospy.Publisher(Path_Plan_Topic, String, queue_size=1, latch=True)
-	pub.publish(msg_json_str)
+# def publish_path_plan(path_start, path_end, master_start_pos, master_goal_pos, follower_start_pos, follower_end_pos):
+# 	# rospy.init_node(Node, anonymous=True)
+# 	angle = get_path_angle(path_start, path_end)
+# 	leader_start = path_start
+# 	leader_end = path_end
+# 	follower_start = offset_coords(path_start, angle)
+# 	follower_end = offset_coords(path_end, angle)
+#
+# 	msg_json_str = json.dumps({"Leader":{'Transfer_Start':leader_start, 'Transfer_End':leader_end,
+# 								'Start':master_start_pos, 'End':master_goal_pos},
+# 								"Follower":{'Transfer_Start': follower_start, 'Transfer_End': follower_end,
+# 								'Start': follower_start_pos, 'End': follower_end_pos}
+# 	})
+# 	pub = rospy.Publisher(Path_Plan_Topic, NavigationTargets, queue_size=1, latch=True)
+# 	pub.publish(msg_json_str)
+#
+# 	print("JSON message published.")
 
-	print("JSON message published.")
+class PathPlanner():
+	def __init__(self):
+		rospy.init_node("path_planner", anonymous=True)
 
-def toFloat(str_list):
-	return str
+		self.master_start_pos = [float(item) for item in rospy.get_param("~master_start_pos").split(' ')]
+		self.master_goal_pos = [float(item) for item in rospy.get_param("~master_goal_pos").split(' ')]
+		self.follower_start_pos = [float(item) for item in rospy.get_param("~follower_start_pos").split(' ')]
+		self.follower_goal_pos = [float(item) for item in rospy.get_param("~follower_goal_pos").split(' ')]
 
-def main():
-	rospy.init_node(Node, anonymous=True)
+		# Get the length of the straight line path
+		self.min_drop_dist = float(rospy.get_param("~min_drop_dist"))
+		self.tb_separation = float(rospy.get_param("~tb_separation"))
 
-	master_start_pos = [float(item) for item in rospy.get_param("~master_start_pos").split(' ')]
-	master_goal_pos = [float(item) for item in rospy.get_param("~master_goal_pos").split(' ')]
-	follower_start_pos = [float(item) for item in rospy.get_param("~follower_start_pos").split(' ')]
-	follower_goal_pos = [float(item) for item in rospy.get_param("~follower_goal_pos").split(' ')]
+		# Get the topic to publish the plans to
+		self.path_topic = rospy.get_param("~path_topic")
 
+		# Initialise message to published and setup initial and goal poses
+		self.nav_targets = NavigationTargets()
+		self.nav_targets.follower.initial = self.follower_start_pos
+		self.nav_targets.follower.goal = self.follower_goal_pos
+		self.nav_targets.leader.initial = self.master_start_pos
+		self.nav_targets.leader.goal = self.master_goal_pos
 
+	def path_planner(self):
+		drop_start = [(self.master_start_pos[0] - self.follower_start_pos[0])/2, (self.master_start_pos[1] - self.follower_start_pos[1])/2]
+		drop_end = [drop_start[0] + self.min_drop_dist, drop_start[1]]
+		X = points_to_list(drop_start, drop_end)
+		P = points_to_list(self.master_start_pos[0:2], self.master_goal_pos[0:2], self.follower_start_pos[0:2], self.follower_goal_pos[0:2])
 
-	min_drop_dist = rospy.get_param("~min_drop_dist")
+		nl_cons = NonlinearConstraint(non_linear_dist_constraint, self.min_drop_dist, 100)
+		options = {'disp':False}
+		ans = minimize(fun=total_dist, args=P, x0=X, method='trust-constr', constraints=nl_cons, options=options)
+		# print(total_dist(ans.x,P))
+		angle = get_path_angle(ans.x[0:2], ans.x[2:4])
 
+		# Store the path start and end
+		path_start = (ans.x[0], ans.x[1], angle)
+		path_end = (and.x[2], ans.x[3], angle)
 
+		# Generate start and end of line for both turtlebots and store in nav message
+		self.nav_targets.follower.line_start = offset_coords(path_start, angle)
+		self.nav_targets.follower.line_start = offset_coords(path_end, angle)
+		self.nav_targets.leader.line_start = path_start
+		self.nav_targets.leader.line_start = path_end
 
-
-
-	path_start, path_end = path_planner(master_start_pos, master_goal_pos, follower_start_pos, follower_goal_pos, min_drop_dist)
-	publish_path_plan(path_start, path_end, master_start_pos, master_goal_pos, follower_start_pos, follower_goal_pos)
-	rospy.spin()
+	def publish_path_plan(self):
+		pub = rospy.Publisher(self.path_topic, NavigationTargets, queue_size=1, latch=True)
+		pub.publish(self.nav_targets)
+		print("Publshed Navigation Plan")
+# 
+# def main():
+# 	rospy.init_node("path_planner", anonymous=True)
+# 	# Get the start and end positions for each robot
+# 	master_start_pos = [float(item) for item in rospy.get_param("~master_start_pos").split(' ')]
+# 	master_goal_pos = [float(item) for item in rospy.get_param("~master_goal_pos").split(' ')]
+# 	follower_start_pos = [float(item) for item in rospy.get_param("~follower_start_pos").split(' ')]
+# 	follower_goal_pos = [float(item) for item in rospy.get_param("~follower_goal_pos").split(' ')]
+#
+# 	# Get the length of the straight line path
+# 	min_drop_dist = rospy.get_param("~min_drop_dist")
+# 	tb_separation = rospy.get_param("~tb_separation")
+# 	path_topic = rospy.get_param("~path_topic")
+#
+# 	# Get the start and end of the straight line path
+# 	path_start, path_end = path_planner(master_start_pos, master_goal_pos, follower_start_pos, follower_goal_pos, min_drop_dist)
+# 	# Publish the path, start and goal positions
+# 	publish_path_plan(path_start, path_end, master_start_pos, master_goal_pos, follower_start_pos, follower_goal_pos)
+# 	rospy.spin()
 
 
 if __name__ == "__main__":
-	main()
-
+	planner = PathPlanner()
+	planner.path_planner()
+	planner.publish_path_plan()
+	rospy.spin()
