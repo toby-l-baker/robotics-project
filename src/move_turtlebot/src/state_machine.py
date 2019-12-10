76fd4574a8c7b598a0c5a50fe93ecac6141f2fa6
liueric101 <liu493@usc.edu>
2019-12-09 19:04:16 -0800

    Modified state machine to get ready checks from all nodes. Additionally publishes follower name to Follow state.

--- src/move_turtlebot/src/state_machine.py
+++ src/move_turtlebot/src/state_machine.py
@@ -9,31 +9,44 @@ class StateMachine():
     packages between TurtleBots.
     """
     def __init__(self):
+        ## Setup
         rospy.init_node('state_machine', anonymous=True)
-
+        # Publishes state on the 'state' topic
         self.state_pub = rospy.Publisher('state', String, queue_size=1);
+
+        # Subscribes to acknowledge messages from each state
         self.initial_ack = rospy.Subscriber('Initial_ack', String, self.initial_callback)
         self.follow_ack = rospy.Subscriber('Follow_ack', String, self.follow_callback)
         self.final_ack = rospy.Subscriber('Final_ack', String, self.final_callback)
-
-        self.initial_ready = False;
-        self.follow_ready = False;
-        self.final_ready = False;
-
-        self.state = state_names.IDLE
-
         # Meta topic - misc control
         meta_sub_topic = rospy.get_param("~meta_sub_topic")
         self.meta_sub = rospy.Subscriber(meta_sub_topic, String, self.meta_callback)
 
 
+        self.leader_name = rospy.get_param("~leader_name")
+        self.follower_name = rospy.get_param("~follower_name")
+
+        # Initializes node ready checks
+        self.path_planner_ready = False;
+        self.leader_move_ready = False;
+        self.follower_move_ready = False;
+        self.follower_follow_ready = False;
+
+        self.state = state_names.IDLE
+        
+        ##State Machine begin
+
+        # Publish IDLE state
         self.state_pub.publish(self.state)
-        while(not(self.initial_ready and self.follow_ready and self.final_ready)):
+        # Wait until all nodes are ready
+        while(not(self.path_planner_ready and self.leader_move_ready and self.follower_move_ready and self.follower_follow_ready)):
             pass
 
-
+        # Go to INITIAL state
         self.state = state_names.INITIAL
         self.state_pub.publish(self.state)
+
+        # Keep listening and updating SM
         rospy.spin()
 
     """
@@ -43,13 +56,17 @@ class StateMachine():
         print(msg.data)
         if msg.data == "stop":
             self.state = state_names.IDLE
+            self.state_pub.publish(self.state)
         if msg.data == "start":
             self.state = state_names.INITIAL
+            self.state_pub.publish(self.state)
         if msg.data == "follow":
             self.state = state_names.FOLLOW
+            self.state_pub.publish(self.state + ' ' + self.follower_name)
+
         if msg.data == "final":
             self.state = state_names.FINAL
-        self.state_pub.publish(self.state)
+            self.state_pub.publish(self.state)
 
     def initial_callback(self, msg):
         if(self.state != state_names.INITIAL):
@@ -60,7 +77,7 @@ class StateMachine():
             print('In Initial State, received message: %s' % msg.data)
             if(msg.data == state_names.DONE):
                 self.state = state_names.FOLLOW
-                self.state_pub.publish(self.state)
+                self.state_pub.publish(self.state + ' ' + self.follower_name) 
                 print("Transitioning to %s" % state_names.FOLLOW)
 
     def follow_callback(self, msg):
